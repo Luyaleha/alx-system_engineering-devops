4679b89c123c3ede7b81936f318ac7de1f300df6
Luyaleha <Luyaleha@gmail.com>
2023-01-20 03:09:55 -0800

    taskess

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,5 +1,8 @@
-# Creates a file
+# create file named school in tmp with below specification
+
 file { '/tmp/school':
+  ensure  => file,
+  path    => '/tmp/school',
   mode    => '0744',
   owner   => 'www-data',
   group   => 'www-data',
